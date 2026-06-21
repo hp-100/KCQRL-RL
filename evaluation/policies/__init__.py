b681af6b6e70106737196229f08f1af79734b56d
hp-100 <852314351@qq.com>
2026-06-21 21:13:59 +0800

    Export NCDM-DDPG policy

--- evaluation/policies/__init__.py
+++ evaluation/policies/__init__.py
@@ -1,7 +1,7 @@
 from .base import BaseCATPolicy, PolicyMetadata
 from .random_policy import RandomPolicy, RandomMIRTPolicy
 from .mirt_policy import HeuristicMIRTPolicy, FormalMIRTPolicy
-from .ddpg_policy import DDPGPolicy
+from .ddpg_policy import DDPGPolicy, NCDMDDPGPolicy, load_lstm_actor_checkpoint
 from .oracle_policy import OneStepOraclePolicy
 
 from .ddpg_mirt_policy import DDPGMIRTPolicy
